ff298d9e3445f1aa4eec878577df250e4748ae1e
skyyy <aakashprajapati2457@gmail.com>
2026-02-12 21:07:33 +0530

    removed old ones

--- Midpointalgo.py
+++ Midpointalgo.py
@@ -1,83 +1,7 @@
 import pygame
-# pygame.init()
-# width, height = 600, 500
-# screen = pygame.display.set_mode((width, height))
-# pygame.display.set_caption("Midpoint Circle Algorithm")
 
-# WHITE = (45, 134, 155)
-# BLACK = (0, 0, 0)
-
-# def draw_circle_points(surface, color, cx, cy, x, y):  
-#     surface.set_at((cx + x, cy + y), color)
-#     surface.set_at((cx - x, cy + y), color)
-#     surface.set_at((cx + x, cy - y), color)
-#     surface.set_at((cx - x, cy - y), color)
-#     surface.set_at((cx + y, cy + x), color)
-#     surface.set_at((cx - y, cy + x), color)
-#     surface.set_at((cx + y, cy - x), color)
-#     surface.set_at((cx - y, cy - x), color)
-
-# def midpoint_circle(surface, color, center, radius):
-#     cx, cy = center
-#     x, y = 0, radius
-#     p = 1 - radius
-
-#     while x <= y:
-#         draw_circle_points(surface, color, cx, cy, x, y)
-#         x += 1
-#         if p < 0:
-#             p += 2 * x + 1
-#         else:
-#             y -= 1
-#             p += 2 * (x - y) + 1
-
-# running = True
-# while running:
-#     screen.fill(WHITE)
+black = (0,255,0)
  
-#     midpoint_circle(screen, BLACK, (300, 250), 100)
-
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             running = False
-
-#     pygame.display.update()
-
-# pygame.quit()
-
-black= (127,50,103)
-# def DRAWCIRCLE(surface , color , cx ,cy , x , y):
-#     surface.set_at((cx + x , cy + y) , color)
-#     surface.set_at((cx - x , cy + y) , color)
-#     surface.set_at((cx + x , cy - y) , color)
-#     surface.set_at((cx - x , cy - y) , color)
-#     surface.set_at((cx + y , cy + x) , color)
-#     surface.set_at((cx - y , cy + x) , color)
-#     surface.set_at((cx + y , cy - x) , color)
-#     surface.set_at((cx - y , cy - x) , color)
-
-# def midCircle(surface , center  , radius):
-#     cx , cy = center
-#     x,y =  0,radius
-#     p = 1-radius
-#     while x <=y:
-#         DRAWCIRCLE(surface , black  , cx , cy , x , y)
-#         x+=1
-#         if p<0:
-#             p += 2 * x +1
-#         else:
-#             y-=1
-#             p += 2 * (x-y)+1
-
-# def DRAWLINE(surface , color  , cx,cy,x,y):
-#     surface.set_at((cx + x , cy + y),color)
-#     surface.set_at((cx - x , cy + y),color)
-#     surface.set_at((cx + x , cy - y),color)
-#     surface.set_at((cx - x , cy - y),color)
-#     surface.set_at((cx + y , cy + x),color)
-#     surface.set_at((cx - y , cy + x),color)
-#     surface.set_at((cx + y , cy - x),color)
-#     surface.set_at((cx - y , cy - x),color)
 def MINPOINT(surface ,colo , cx,cy , x,y):
     surface.set_at((cx+x , cy+y) , colo)
     surface.set_at((cx-x , cy+y) , colo)
@@ -96,7 +20,7 @@ def secondfunction(surface , center , radius):
         MINPOINT(surface ,black , cx , cy,x,y)
         x+=1
         if p<0:
-            p += 2 * x+100
+            p += 2 * x+1
         else:
             y -=1
             p+= 2 * (x-y)+1

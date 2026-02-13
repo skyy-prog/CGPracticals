983c49152774e6dba4f38b47f7726de2cbc1ac34
skyyy <aakashprajapati2457@gmail.com>
2026-02-14 01:48:29 +0530

    practiced

--- SheaningTransformation.py
+++ SheaningTransformation.py
@@ -1,37 +1,27 @@
 import pygame
 import numpy as np
 
-def shear(points, shx, shy, pivot=None):
-    if pivot is None:
-        pivot = (0, 0)
-
-    px, py = pivot
-
  
+
+def shear(points , shx,shy , pivot = None):
+    if pivot is None:
+        pivot = (0,0)
+    px,py = pivot
     shearing = np.array([
-        [1, shx, -py * shx],
-        [shy, 1, -px * shy],
-        [0, 0, 1]
+        [1,shx, -py * shx],
+        [shy ,1 , -px * shy],
+        [0,0,1]
     ])
-
-     
-    homoPoints = [np.array([x, y, 1]) for x, y in points]
-
-     
+    homoPoints = [np.array([x,y,1]) for x,y in points]
     transPoint = [shearing @ p for p in homoPoints]
-
-    return [(int(p[0]), int(p[1])) for p in transPoint]
-
+    return [(int(p[0]) , int(p[1])) for p in transPoint]
 
 pygame.init()
 screen = pygame.display.set_mode((500, 500))
 pygame.display.set_caption('Shearing Points')
 
- 
-OGPOINTS = [(200, 200), (300, 200), (200, 0)]
-
- 
-sharedPoint = shear(OGPOINTS, 0.5, 0.2, (250, 250))
+OGPOINTS = [(200,200) , (300,200) , (200,0)]
+sharedPoint = shear(OGPOINTS , 0.10,0.10 ,  (250,250))
 
 running = True
 while running:
@@ -42,9 +32,10 @@ while running:
     screen.fill((205, 255, 255))
  
     pygame.draw.polygon(screen, (0, 0, 0), OGPOINTS, 2)
- 
     pygame.draw.polygon(screen, (255, 0, 0), sharedPoint, 2)
-
+    
     pygame.display.flip()
 
 pygame.quit()
+
+  
